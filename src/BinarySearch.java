c295366eaf96995e81cb1e1d8d930f476bfcf61d
Pranay Kumar <PranayKumarSoni@gmail.com>
2023-12-03 23:56:31 +0530

    LC#704: Binary Search

---
+++ src/BinarySearch.java
@@ -0,0 +1,25 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 9;
+        Solution solution = new Solution();
+        System.out.println(solution.search(nums, target));
+    }
+
+    static class Solution {
+        public int search(int[] nums, int target) {
+            int l = 0, r = nums.length - 1;
+            while (l<=r) {
+                int mid = l + ((r-l)/2);
+                if(nums[mid] == target) {
+                    return mid;
+                } else if (nums[mid]>target) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
